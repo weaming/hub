8e477d495aeaa798cf3b8ff25022bd60163948f2
weaming <garden.yuen@gmail.com>
2020-02-23 15:12:34 +0800

    update readme

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 * `Topic` will send `Message` to all subscribers under it.
     * One subscriber maybe receive same messge for more than one times.
 * Every subscriber will subscribe the "global" `Topic`.
+
+## Core structures
+
+```go
+type PubMessage struct {
+	Type         string        `json:"type"`
+	Data         string        `json:"data"`          // string or base64 of bytes
+	ExtendedData []string      `json:"extended_data"` // string or base64 of bytes, for sending multiple photos
+	Captions     []string      `json:"captions"`      // all captions of Data and ExtendedData
+}
+```
--- example/hub.py
+++ example/hub.py
@@ -3,6 +3,7 @@ import json
 from functools import partial
 from enum import Enum
 from base64 import b64encode
+
 try:
     import thread
 except ImportError:
@@ -55,26 +56,41 @@ def data_to_str(data, type):
         return json.dumps(data, ensure_ascii=False)
     if type in [MESSAGE_TYPE.IMAGE.name]:
         if not isinstance(data, str):
-            return b64encode(data).encode('utf8')
+            return b64encode(data).decode('utf8')
     return str(data)
 
 
-def new_pub_message(data, type=MESSAGE_TYPE.PLAIN.name, topics=('global', )):
+def new_pub_message(
+    data,
+    type=MESSAGE_TYPE.PLAIN.name,
+    topics=('global',),
+    extended_data=[],
+    captions=[],
+):
     return {
         'action': "PUB",
         'topics': topics,
         'message': {
             'type': type,
             'data': data_to_str(data, type),
-        }
+            'extended_data': [data_to_str(x, type) for x in extended_data],
+            'captions': captions,
+        },
     }
 
 
-def http_post_data_to_hub(data, topics):  # type: str
+def http_post_data_to_hub(data, topics, type=MESSAGE_TYPE.JSON.name):
+    msg = new_pub_message(data, type=type, topics=topics)
+    return requests.post("https://hub.drink.cafe/http", json=msg)
+
+
+def http_post_photos_to_hub(data: list, topics, captions=[]):
     msg = new_pub_message(
-        data,
-        type=MESSAGE_TYPE.JSON.name,
-        topics=topics
+        data[0],
+        type=MESSAGE_TYPE.IMAGE.name,
+        topics=topics,
+        extended_data=data[1:],
+        captions=captions,
     )
     return requests.post("https://hub.drink.cafe/http", json=msg)
 
@@ -82,3 +98,38 @@ def http_post_data_to_hub(data, topics):  # type: str
 def sub_topics(ws, topics):
     msg = {'action': "SUB", "topics": topics}
     ws.send(json.dumps(msg, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    """
+    e.g. python utils/message_hub.py hello -t test admin/ping
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topics", nargs='+')
+    parser.add_argument("--text", help="message text which will be send")
+    parser.add_argument("--photo", nargs='*', help="path of photos")
+    parser.add_argument(
+        "--caption", nargs='*', help="captions of photos, missing at tail is allowed"
+    )
+    args = parser.parse_args()
+    if args.photo:
+        print(
+            http_post_photos_to_hub(
+                [
+                    x if x.startswith('http') else open(x, 'rb').read()
+                    for x in args.photo
+                ],
+                args.topics,
+                captions=args.caption,
+            ).text
+        )
+    else:
+        print(
+            http_post_data_to_hub(
+                args.text.replace("\\n", "\n"),
+                args.topics,
+                type=MESSAGE_TYPE.PLAIN.name,
+            ).text
+        )
